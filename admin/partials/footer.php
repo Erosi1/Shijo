259bc9e8713e85a46b73f6471912652991b592eb
Erosi1 <em52473@ubt-uni.net>
2023-08-09 16:05:40 +0200

    html and css design

---
+++ admin/partials/footer.php
@@ -0,0 +1,7 @@
+<!-- Footer Section  Starts-->
+<div class="footer">
+          <div class="wrapper">
+            <p class="text-center">2023 All rights reserved, Shijo . Developed by Eros Mehmeti</p>
+            </div>
+
+           </div>
